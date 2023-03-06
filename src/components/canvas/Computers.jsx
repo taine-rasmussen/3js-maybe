86b20f4786fa1aa6caa0a9d355745a46402c7b45
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-06 21:02:45 +0000

    changing version / begging for it to work

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,21 +1,30 @@
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import * as THREE from 'three';
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scence.glft')
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight
-        intensity={0.15}
-        groundColor='black'
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
       />
       <pointLight intensity={1} />
       <primitive
-        obeject={computer.scene} />
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   )
 }
