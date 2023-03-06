b17a15eeb55cc8576df81530214f4d1db6c5a5fa
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-06 18:17:35 +0000

    name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,10 @@ const Navbar = () => {
             window.scrollTo(0, 0)
           }}
         >
-          <img src={logo} alt='logo' className='w-9' />
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>
+            Taine <span className='sm:block hidden'>| Rasmussen</span>
+          </p>
         </Link>
       </div>
 
