3f4157ba6586cf2ed2539c881028365fdfb8e770
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-06 18:31:45 +0000

    add icon / logic for close

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,11 +45,10 @@ const Navbar = () => {
         </ul>
         <div className='sm:hidden flex flex-1 justify-end items center'>
           <img
-            src={menu}
+            src={toggle ? close : menu}
             alt='menu'
             className='w-[28px] h-[28px] object-contain cursor-pointer'
             onClick={() => { setToggle(!toggle) }}
-
           />
 
         </div>
