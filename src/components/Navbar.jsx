60ffd5fcdf93236ae41e79bf97df03ae7bf82acf
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-06 18:04:27 +0000

    placeholder logo img:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,26 @@ import { navLinks } from "../constants";
 import { styles } from "../styles";
 
 const Navbar = () => {
+
+  const [active, setActive] = useState('')
+
+
   return (
     <div className={`${styles.paddinX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className='w-full flex justify-between items-center max-1-7x1 mx-auto'>
-
+        <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive('')
+            window.scrollTo(0, 0)
+          }}
+        >
+          <img src={logo} alt='logo' className='w-9' />
+        </Link>
       </div>
 
-    </div>
+    </div >
   )
 }
 
