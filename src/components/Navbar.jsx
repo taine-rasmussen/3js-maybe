1510cc5c4ad55296637902401b3273894c06f28a
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-06 17:57:53 +0000

    a mess

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,16 @@
 
 
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-
-import { styles } from "../styles";
-import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 import { useEffect, useState } from "react";
-import { navLinks } from "../constants";
 import { Link } from "react-router-dom";
+import { navLinks } from "../constants";
 import { styles } from "../styles";
 
 const Navbar = () => {
   return (
-    <div className>
+    <div className={
+      `${styles.paddinX}`
+    }>
 
     </div>
   )
