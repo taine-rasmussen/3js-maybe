78d96be1baf92696d7326f12337ce3ee148aa97d
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-06 18:26:51 +0000

    more styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,9 +32,9 @@ const Navbar = () => {
             <li
               key={link.id}
               className={`${active === link.title
-                  ? 'text-white'
-                  : 'text-secondary'
-                }`}
+                ? 'text-white'
+                : 'text-secondary'
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
             >
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
