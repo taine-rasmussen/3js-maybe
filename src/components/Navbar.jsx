9d3c2f52214aee653a7b420c55ef2aacc3706cf0
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-06 17:55:53 +0000

    navBar imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+
+
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
+import { logo, menu, close } from "../assets";
+import { useEffect, useState } from "react";
+import { navLinks } from "../constants";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className>
+
+    </div>
   )
 }
 
