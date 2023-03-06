5f5ff151eea29448b02187f722256f4a580c42b3
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-06 17:56:14 +0000

    remove double up import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,6 @@ import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
-import { logo, menu, close } from "../assets";
 import { useEffect, useState } from "react";
 import { navLinks } from "../constants";
 import { Link } from "react-router-dom";
