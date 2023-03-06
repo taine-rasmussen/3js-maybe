fbc513741a4d1e8206652001266aba9f9a652e13
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-06 18:32:22 +0000

    reduce img height to 9px

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
             window.scrollTo(0, 0)
           }}
         >
-          <img src={logo} alt='logo' className='w-12 h-12 object-contain' />
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer'>
             Taine Rasmussen<span className='sm:block hidden'>| Portfolio</span>
           </p>
