79e4644f94ae7cfea004e76f609eeec605e276b5
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-07 10:25:31 +0000

    yuh

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,45 @@
 
 import { fadeIn, textVariant } from "../utils/motion";
-import { SectionWrapper } from "../hoc";
 import { services } from "../constants";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import Tilt from "react-tilt";
 
 
+const ServiceCard = (props) => {
+  const {
+    index,
+    title,
+    icon
+  } = props
+
+  return (
+    <p>{title}</p>
+  )
+}
+
+
 const About = () => {
   return (
     <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-2-3x1 leading-[30px]'
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, i) => (
+          <ServiceCard key={service.title} index={i} {...service} />
+        ))}
+
+      </div>
     </>
   )
 }
