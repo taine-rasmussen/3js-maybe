3ca309e7bee87a337e83dd748cae58117b578398
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-07 10:15:50 +0000

    imports

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+import { services } from "../constants";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import Tilt from "react-tilt";
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+
+    </>
   )
 }
 
