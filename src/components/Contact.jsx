c39e794d1d7af61809dc7609e48dbf3d6ea0f2cc
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-07 15:05:13 +0000

    header slides in

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,9 +22,16 @@ const Contact = () => {
 
   return (
     <div className='xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden'>
+      <motion.div
+        variants={slideIn('left', 'tween', 0.2, 1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
 
+      </motion.div>
     </div>
   )
 }
 
-export default SectionWrapper(Contact, '')
+export default SectionWrapper(Contact, 'contact')
