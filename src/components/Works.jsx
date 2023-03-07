669f02a2e0d07aa3ad66864d104b4779765075d8
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-07 14:16:56 +0000

    github icon

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -36,10 +36,17 @@ const ProjectCard = (props) => {
             className='w-full h-full object-cover rounded-2xl'
           />
 
-          <div
-            className='absolute inset-0 flex justify-end m-3 card-img_hover'
-            onClick={() => window.open(source_code_link, '_blank')}
-          >
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div
+              onClick={() => window.open(source_code_link, '_blank')}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src={github}
+                alt='github'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
 
           </div>
         </div>
