42e752170a8bc40c75dda45ad2b2f6802816c237
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-07 13:13:18 +0000

    layout?

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,6 +1,5 @@
 import { motion } from 'framer-motion';
 import Tilt from 'react-tilt';
-import React from 'react';
 
 import { fadeIn, textVariant } from '../utils/motion';
 import { projects } from '../constants';
@@ -11,8 +10,25 @@ import { github } from '../assets';
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div
+        vvariants={textVariant()}
+      >
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+
+      <div className='w-full flex'>
+        <motion.p
+          variants={fadeIn('', '', 0.1, 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3x1 leading-[30px]'
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </motion.p>
+
+      </div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, '')
