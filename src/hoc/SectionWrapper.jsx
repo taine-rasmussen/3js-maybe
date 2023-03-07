877bd81d2b3f0b0f9e7f730c441e96a05de7c7b8
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-07 10:39:37 +0000

    imports

---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,13 @@
+import { staggerContainer } from "../utils/motion";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+
+const SectionWrapper = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default SectionWrapper
