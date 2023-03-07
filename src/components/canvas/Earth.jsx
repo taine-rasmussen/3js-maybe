0441b1d7565c8f52d905ab8842ad8355d6365e7a
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-07 15:22:39 +0000

    lil guy

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,14 +1,19 @@
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { Suspense } from 'react';
 
 import CanvasLoader from '../Loader';
 
 const Earth = () => {
+
+  const earth = useGLTF('./planet/scene.gltf')
   return (
-    <div>
-      Earth
-    </div>
+    <primitive
+      object={earth.scene}
+      scale={2.5}
+      position-y={0}
+      rotation-y={0}
+    />
   )
 }
 
@@ -18,7 +23,12 @@ const EarthCanvas = () => {
       shadows
       frameLoop='demand'
       gl={{ preserveDrawingBuffer: true }}
-      camera={{}}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6]
+      }}
     >
       <Suspense fallback={<CanvasLoader />} />
       <OrbitControls
