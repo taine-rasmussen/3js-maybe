1fcad063f6c20942bdb1c4882b749da8a1a7af36
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-07 10:12:36 +0000

    better position for scroll on mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,8 +23,7 @@ const Hero = () => {
 
       <ComputersCanvas isMobile={isMobile} setIsMobile={setIsMobile} />
 
-      {/* {!isMobile && ( */}
-      <div className='absolute xs:bottom-30 bottom-32 w-full flex justify-center items-center'>
+      <div className={`absolute ${isMobile ? 'bottom-12' : 'bottom-32'} w-full flex justify-center items-center`}>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
@@ -41,7 +40,6 @@ const Hero = () => {
           </div>
         </a>
       </div>
-      {/* )} */}
     </section>
   )
 }
