40a8254a14c0afb1f9290b48cc4249ce707e662d
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-07 11:30:49 +0000

    imports;'

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,15 @@
-import React from 'react'
+import "react-vertical-timeline-component/style.min.css";
+import { motion } from "framer-motion";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
 
 const Experience = () => {
   return (
